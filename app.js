682964ff7ddaacc4a07da023757c655448b9b374
Tarik oz <a.tarik2005@gmail.com>
2022-09-13 14:44:23 +0300

    checkBox

--- app.js
+++ app.js
@@ -51,12 +51,12 @@ if(e.key === 'Enter'){
     btn.click();}
 })
 
-// *************remove*****
+// *************remove**********
 
 todo.addEventListener("click",(e)=>{
 if(e.target.classList.contains("remove1")){e.target.parentElement.parentElement.remove();}
 })
-
+// *************line through************
 todo.addEventListener("click", (e) => {
   if (e.target.classList.contains("box")) {
     e.target.nextElementSibling.style.textDecoration= "line-through";
@@ -68,3 +68,43 @@ todo.addEventListener("click", (e) => {
 
 
 
+// *************checkBox************
+
+// todo.addEventListener("click", (e) => {
+//   let sayac;
+
+//   if(e.target.classList.contains("box")){
+//     sayac += 1;
+//     console.log(sayac)
+
+//     if(sayac % 2 === 1){ 
+//           sayac += 1;
+//           console.log(sayac)
+//         e.target.nextElementSibling.style.textDecoration = "line-through";
+  
+    
+//     console.log(sayac)
+//     }else if(sayac%2 == 0){
+//         e.target.nextElementSibling.style.textDecoration = "none";
+//     }
+  
+  
+  
+   
+//   }
+// });
+
+
+for (i = 0; i < 100; i++) {
+  let sayac = 1;
+  todo.addEventListener("click", (e) => {
+    if (e.target.classList.contains("box")) {
+      if (sayac % 2 == 1) {
+        e.target.nextElementSibling.style.textDecoration = "line-through";
+      } else if (sayac % 2 == 0) {
+        e.target.nextElementSibling.style.textDecoration = "none";
+      }
+      sayac += 1;
+    }
+  });
+}
